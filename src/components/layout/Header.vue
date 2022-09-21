bceb7e05d684b59b2a9b2baffee9ddaaca9c58eb
QuentinHeinis <94958477+QuentinHeinis@users.noreply.github.com>
2022-09-21 11:01:11 +0200

    modif header

---
+++ src/components/layout/Header.vue
@@ -0,0 +1,48 @@
+<script setup>
+import { ChevronDownIcon } from '@heroicons/vue/24/outline'
+</script>
+
+<template>
+    <header class="flex justify-between items-center w-screen text-white lg:px-40 z-20 h-[82px] relative">
+        <div class="w-[177px] absolute">
+            <img src="/images/logos/CRBX_Logo.png" alt="Logo CRBX">
+        </div>
+        <nav class="fixed right-40 ">
+            <ul class="flex gap-6">
+                <li>create</li>
+                <li>socials</li>
+                <li class="flex items-center gap-1 relative liste_container">collections
+                    <ChevronDownIcon class="w-3 chevron" />
+                    <ul class="absolute w-full liste">
+                        <li>offf</li>
+                        <li>mmi</li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<style>
+nav .chevron {
+    transition: rotate .4s ease;
+}
+
+nav .liste_container:hover .chevron {
+    rotate: 180deg;
+}
+
+nav .liste {
+    visibility: hidden;
+    opacity: 0;
+    bottom: 0;
+    transition: .4s ease-in-out;
+}
+
+nav .liste_container:hover .liste {
+    visibility: visible;
+    opacity: 1;
+    bottom: -200%;
+    transition: .4s ease-in-out;
+}
+</style>
