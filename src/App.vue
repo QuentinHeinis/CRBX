8cd3addacdf57dabaf62efae498dbf9bed8fb71b
QuentinHeinis <94958477+QuentinHeinis@users.noreply.github.com>
2022-10-12 09:01:25 +0200

    fix background

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,23 @@ import Header from './components/layout/Header.vue';
 <template>
 
     <Header />
-    <main class="min-h-screen pt-32 z-10">
+    <main class="min-h-screen pt-32 z-10 relative">
         <RouterView />
     </main>
 </template>
+
+<style>
+main::after {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    height: calc(100% - 100vh);
+    width: 100%;
+    display: block;
+    box-sizing: border-box;
+    background-image: url('/images/bg.webp');
+    background-size: contain;
+    z-index: -10;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,9 @@
 <script setup>
 import Light from '../components/UI_Kit/deco/Lights/Light.vue';
-import Background from '../components/UI_Kit/deco/Background.vue';
 import FullBtn from '../components/UI_Kit/Buttons/FullBtn.vue';
 import BorderBtn from '../components/UI_Kit/Buttons/BorderBtn.vue';
 import Stars from '../components/UI_Kit/deco/Stars.vue';
 import HomeCards from '../components/UI_Kit/Cards/HomeCards.vue';
-import Circle from '../components/UI_Kit/deco/Circle.vue';
 import { ChevronDownIcon } from '@heroicons/vue/24/outline';
 import ItemCards from '../components/UI_Kit/Cards/ItemCards.vue';
 import CircleText from '../components/UI_Kit/deco/CircleText.vue';
@@ -38,7 +36,6 @@ import CircleText from '../components/UI_Kit/deco/CircleText.vue';
             </div>
             <CircleText class="hidden md:flex -translate-y-16 translate-x-10" />
         </section>
-        <Background class="pt-40" />
         <section>
             <div class="text-white my-28 relative flex w-full justify-center">
                 <h2 class="flex justify-center text-4xl font-light gap-2 py-2 px-1 ">
@@ -47,8 +44,6 @@ import CircleText from '../components/UI_Kit/deco/CircleText.vue';
                         CRBX<span class="text-xl relative -top-2">&copy;</span>
                     </span>
                 </h2>
-                <Circle
-                    class="hidden md:block w-60 absolute h-60 stroke-white -top-28 right-1/2 translate-x-[85%] translate-y-4 rotate-12" />
             </div>
             <div class="flex h-fit flex-wrap justify-around gap-x-[5%] gap-y-7">
                 <HomeCards :title="'Create your own NFT using the CRBX tool'">CRBX use his own intuitive and creativity
@@ -70,9 +65,6 @@ import CircleText from '../components/UI_Kit/deco/CircleText.vue';
                         CRBX<span class="text-xl relative -top-2">&copy;</span>
                     </span>
                     collection
-                    <!-- <span
-                        class="bg-gradient-to-l from-[#555CC5] via-[#676DCA] to-[#6C72CB] h-2 w-full absolute bottom-0 -z-10 left-9">
-                    </span> -->
                 </h2>
             </div>
             <div class="flex flex-wrap justify-center gap-x-[5%] gap-y-7">
@@ -91,7 +83,7 @@ import CircleText from '../components/UI_Kit/deco/CircleText.vue';
 
             <div
                 class="mt-28 grid grid-cols-[repeat(auto-fit,minmax(300px,1fr))] w-4/5 lg:w-full gap-x-7 gap-y-10 mx-auto">
-                <ItemCards />
+                <ItemCards :FromCollection="false" :Collection="'machin'" />
                 <ItemCards :Img="'/images/temporary/Image-1.png'" />
                 <ItemCards :Img="'/images/temporary/Image-2.png'" />
                 <ItemCards :Img="'/images/temporary/Image-3.png'" />
