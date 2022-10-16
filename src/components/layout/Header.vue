9ebd5554ed773d1108c1ecfc77cca78a391416d6
QuentinHeinis <94958477+QuentinHeinis@users.noreply.github.com>
2022-10-16 19:55:17 +0200

    link added

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -9,7 +9,7 @@ const switchMenu = () => {
 
 <template>
     <header class="flex justify-between items-center w-screen text-white px-9 lg:px-40 z-20 h-16 lg:h-[82px] relative">
-        <router-link to="/" class="w-24 hidden md:w-[177px]">
+        <router-link to="/" class="w-24 hidden md:block md:w-[177px]">
             <img src="/images/logos/CRBX_Logo.png" alt="Logo CRBX">
         </router-link>
         <nav class="fixed right-4 md:right-9 lg:right-40 ">
@@ -18,16 +18,26 @@ const switchMenu = () => {
                 <span class="w-2/3 h-[2px] bg-white self-end"></span>
             </div>
             <ul class="hidden md:flex gap-6">
-                <li>create</li>
-                <li>socials</li>
-                <RouterLink to="/three">
-                    threeJs
-                </RouterLink>
+                <li>
+                    <RouterLink to="/create">create</RouterLink>
+                </li>
+                <li>
+                    <RouterLink to="/socials">socials</RouterLink>
+                </li>
+                <li>
+                    <RouterLink to="/three">
+                        threeJs
+                    </RouterLink>
+                </li>
                 <li class="flex items-center gap-1 relative liste_container">collections
                     <ChevronDownIcon class="w-3 chevron" />
                     <ul class="absolute w-full liste">
-                        <li>offf</li>
-                        <li>mmi</li>
+                        <li>
+                            <RouterLink to="/collection/offf">offf</RouterLink>
+                        </li>
+                        <li>
+                            <RouterLink to="/collection/mmi">mmi</RouterLink>
+                        </li>
                     </ul>
                 </li>
             </ul>
--- src/router/index.js
+++ src/router/index.js
@@ -17,7 +17,11 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
     },
-    { path: '/three', name: 'three', component: three }
+    { path: '/three', name: 'three', component: three },
+    { path: '/collection/:id', name: 'collection', component: () => import('../views/collections/CollectionView.vue') },
+    { path: '/socials', name: 'socials', component: () => import('../views/SocialsView.vue') },
+    { path: '/Login', name: 'Login', component: () => import('../views/LoginView.vue') },
+    { path: '/Create', name: 'Create', component: () => import('../views/CreateView.vue') },
   ]
 })
 
